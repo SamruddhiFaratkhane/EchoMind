baaa216e4f345fe3cd2be6451975cd64460957da
Samruddhi Faratkhane <samruddhif23@gmail.com>
2025-06-20 11:51:31 +0530

    Delete ztrial.py

--- ztrial.py
+++
@@ -1,9 +0,0 @@
-# import shutil
-# print(shutil.which("ffmpeg"))
-
-#import os
-
-#audio_path = r"G:\mental_health_assessment\temp_audio.mp3"  # or .mp3
-#print("Audio file exists:", os.path.exists(audio_path))
-
-#st.write(df.head())  # This will print the dataframe in Streamlit
